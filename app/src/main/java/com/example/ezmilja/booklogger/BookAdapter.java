69f9af55b98d5657f5d26cfb29d2ca74ebb35e61
paulkirwan6 <39994963+paulkirwan6@users.noreply.github.com>
2018-08-09 14:16:54 +0100

    Scrapped

--- app/src/main/java/com/example/ezmilja/booklogger/BookAdapter.java
+++ app/src/main/java/com/example/ezmilja/booklogger/BookAdapter.java
@@ -7,51 +7,20 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Filter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import static com.example.ezmilja.booklogger.ContentsActivity.books;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ImageViewHolder> {
-    private Book[] images;
+
+
     private Context context;
-    private BooksAdapterListener listener;
-    private List<Book> bookList;
-    private List<Book> bookListFiltered;
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    BookAdapter(Context context_) {
 
-        public TextView name;
-        public ImageView thumbnail;
-
-        MyViewHolder(View view) {
-            super(view);
-            name = view.findViewById(R.id.bookDetails);
-            thumbnail = view.findViewById(R.id.imageViewCustom);
-
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                    // send selected book row in callback
-                    listener.onBookSelected(bookListFiltered.get(getAdapterPosition()));
-                }
-            });
-        }
-    }
-
-    BookAdapter(Book[] books, Context context_, List<Book> bookList) {
-
-        this.images = books;
         this.context = context_;
-        this.listener = listener;
-        this.bookList = bookList;
-        this.bookListFiltered = bookList;
     }
 
     @NonNull
@@ -64,33 +33,27 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ImageViewHolde
     @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
-         {
-            try {
-                if (books[position].imageAddress != null)
-                {
-                    Glide.with(this.context)
-                            .load(books[position].imageAddress)
-                            .diskCacheStrategy(DiskCacheStrategy.ALL)
-                            .into(holder.getImage());
 
-                    holder.BookDetails.setText(books[position].bookName + "\n" + books[position].author + "\n" + books[position].genre);
+        //BookAdapter.bind(items.get(position), clickListener);
+        try {
+            if (books[position].imageAddress != null)
+            {
+                Glide.with(this.context)
+                        .load(books[position].imageAddress)
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .into(holder.getImage());
+
+                holder.BookDetails.setText(books[position].bookName + "\n" + books[position].author + "\n" + books[position].genre);
                 }
-
-                if (books[position].imageAddress == null){
-                    System.out.println("fu");
-                }
-
-            }catch (NullPointerException e){
-                e.printStackTrace();
-                System.out.println("REEEEEEEEEEEEEEEEEEEEEeeeeee" + e );
-
-            }
+                }catch (NullPointerException e){
+            e.printStackTrace();
+            System.out.println("Null Error = " + e );
         }
     }
 
     @Override
     public int getItemCount() {
-        return images.length;
+        return books.length;
     }
 
     public static class ImageViewHolder extends RecyclerView.ViewHolder{
@@ -107,42 +70,5 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ImageViewHolde
         public ImageView getImage(){
             return this.BookImage;
         }
-    }
-
-    public Filter getFilter() {
-        return new Filter() {
-            @Override
-            protected FilterResults performFiltering(CharSequence charSequence) {
-                String charString = charSequence.toString();
-                if (charString.isEmpty()) {
-                    bookListFiltered = bookList;
-                } else {
-                    List<Book> filteredList = new ArrayList<>();
-                    for (Book row : bookList) {
-
-                        // name match condition. this might differ depending on your requirement
-                        // here we are looking for name match
-                        if (row.getName().toLowerCase().contains(charString.toLowerCase())) {
-                            filteredList.add(row);
-                        }
-                    }
-                    bookListFiltered = filteredList;
-                }
-
-                FilterResults filterResults = new FilterResults();
-                filterResults.values = bookListFiltered;
-                return filterResults;
-            }
-
-            @Override
-            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
-                bookListFiltered = (ArrayList<Book>) filterResults.values;
-                notifyDataSetChanged();
-            }
-        };
-    }
-
-    public interface BooksAdapterListener {
-        void onBookSelected(Book bookRow);
     }
 }
